bcdf5db0dce8246c3f4b50c17ee304d86a706594
Andrey Stoyan <stoyan.yukio@gmail.com>
2019-03-27 20:10:38 +0300

    Main updated

--- game/main.cpp
+++ game/main.cpp
@@ -1,16 +1,28 @@
 #include <QGuiApplication>
-#include <QQmlApplicationEngine>
+#include <QtGui/QOpenGLContext>
+#include <QtQml/QQmlEngine>
+#include <QtQuick/QQuickView>
+
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
-
     QGuiApplication app(argc, argv);
 
-    QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
-    if (engine.rootObjects().isEmpty())
-        return -1;
+    QSurfaceFormat format;
+    if (QOpenGLContext::openGLModuleType() == QOpenGLContext::LibGL) {
+        format.setVersion(3, 2);
+        format.setProfile(QSurfaceFormat::CoreProfile);
+    }
+    format.setDepthBufferSize(24);
+    format.setStencilBufferSize(8);
 
-    return app.exec();
+    QQuickView view;
+    view.setFormat(format);
+    view.setResizeMode(QQuickView::SizeRootObjectToView);
+    QObject::connect(view.engine(), &QQmlEngine::quit,
+                     &app, &QGuiApplication::quit);
+    view.setSource(QUrl("qrc:/main.qml"));
+    view.show();
+
+    return QGuiApplication::exec();
 }
