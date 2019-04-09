8931fa41e77bbb05c3caf4054b869649ec80c9a7
Andrey Stoyan <stoyan.yukio@gmail.com>
2019-04-10 02:33:21 +0300

    Main updated

--- game/main.cpp
+++ game/main.cpp
@@ -1,36 +1,6 @@
-#include <QGuiApplication>
+#include "stdafx.hpp"
 
-#include <Qt3DRender/qcamera.h>
-#include <Qt3DCore/qentity.h>
-#include <Qt3DRender/qcameralens.h>
-
-#include <QtWidgets/QApplication>
-#include <QtWidgets/QWidget>
-#include <QtWidgets/QHBoxLayout>
-#include <QtWidgets/QCheckBox>
-#include <QtWidgets/QCommandLinkButton>
-#include <QtGui/QScreen>
-
-#include <Qt3DInput/QInputAspect>
-
-#include <Qt3DExtras/qtorusmesh.h>
-#include <Qt3DRender/qmesh.h>
-#include <Qt3DRender/qtechnique.h>
-#include <Qt3DRender/qmaterial.h>
-#include <Qt3DRender/qeffect.h>
-#include <Qt3DRender/qtexture.h>
-#include <Qt3DRender/qrenderpass.h>
-#include <Qt3DRender/qsceneloader.h>
-#include <Qt3DRender/qpointlight.h>
-
-#include <Qt3DCore/qtransform.h>
-#include <Qt3DCore/qaspectengine.h>
-
-#include <Qt3DRender/qrenderaspect.h>
-#include <Qt3DExtras/qforwardrenderer.h>
-
-#include <Qt3DExtras/qt3dwindow.h>
-#include <Qt3DExtras/qfirstpersoncameracontroller.h>
+#include "game.hpp"
 
 
 int main(int argc, char * argv[])
@@ -39,6 +9,9 @@ int main(int argc, char * argv[])
 
     Qt3DExtras::Qt3DWindow * view = new Qt3DExtras::Qt3DWindow();
     view->defaultFrameGraph()->setClearColor(QColor(QRgb(0x2D9EB5)));
+
+    Qt3DCore::QEntity * rootEntity = new Qt3DCore::QEntity();
+    view->setRootEntity(rootEntity);
 
     QWidget * container = QWidget::createWindowContainer(view);
     QSize screenSize = view->screen()->size();
@@ -49,7 +22,10 @@ int main(int argc, char * argv[])
     QHBoxLayout * hLayout = new QHBoxLayout(widget);
     hLayout->addWidget(container);
 
+//    view->registerAspect(new CustomAspect);
 
+
+    Game::instance().init(rootEntity, view->camera());
 
     widget->setWindowTitle(QStringLiteral("Brain tennis"));
     widget->show();
