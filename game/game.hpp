3b95692c0b2fa2b40a3257ffe0d195b6a6affecb
Andrey Stoyan <stoyan.yukio@gmail.com>
2019-04-10 02:30:37 +0300

    Game class added

---
+++ game/game.hpp
@@ -0,0 +1,31 @@
+#ifndef ENGINE_HPP
+#define ENGINE_HPP
+
+#include "stdafx.hpp"
+
+#include "scene.hpp"
+
+
+class Game final {
+public:
+    static Game & instance();
+
+    void init(Qt3DCore::QEntity * rootEntity, Qt3DRender::QCamera * camera);
+
+private:
+    Game() = default;
+
+private:
+    Qt3DCore::QEntity   * rootEntity_ = nullptr;
+    Qt3DRender::QCamera * camera_     = nullptr;
+    std::unique_ptr<Scene> scene_;
+};
+
+
+inline Game & Game::instance() {
+    static Game e;
+    return e;
+}
+
+
+#endif // ENGINE_HPP
--- game/game.pro
+++ game/game.pro
@@ -14,10 +14,14 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-SOURCES += main.cpp
+SOURCES += main.cpp \
+    scene.cpp \
+    game.cpp
 
 HEADERS += \
-    stdafx.hpp
+    stdafx.hpp \
+    scene.hpp \
+    game.hpp
 
 PRECOMPILED_HEADER = stdafx.hpp
 
