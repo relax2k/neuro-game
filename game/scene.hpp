d6f477f50403cff7898197ca5dc3de0c6215afc3
Andrey Stoyan <stoyan.yukio@gmail.com>
2019-05-17 21:38:34 +0300

    Table class added

--- game/scene.hpp
+++ game/scene.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "stdafx.hpp"
+#include "table.hpp"
 
 #define ASSETS "qrc:/assets/"
 
@@ -11,29 +12,23 @@ class Scene final {
 public:
     explicit Scene(Qt3DCore::QEntity * root);
 
-    std::optional<QVector3D> intersectsWithTable(Ball const * ball) const;
-    std::optional<QVector3D> intersectsWithGrid (Ball const * ball) const;
+    Table const * table() const;
 
 public:
     constexpr static const float SCALE = 0.005f;
 
 private:
-    bool inTable(QVector3D pos) const;
-
-    Qt3DCore::QEntity * createTable()  const;
     Qt3DCore::QEntity * createRoom()   const;
     Qt3DCore::QEntity * createCarpet() const;
     QVector<Qt3DCore::QEntity *> createLights() const;
     Qt3DCore::QEntity * createLight(QVector3D pos) const;
     Qt3DCore::QEntity * createCeiling() const;
-    Qt3DCore::QEntity * createGrid() const;
 
 private:
     Qt3DCore::QEntity * rootEntity_;
-    Qt3DCore::QEntity * table_;
     Qt3DCore::QEntity * room_;
     Qt3DCore::QEntity * carpet_;
     Qt3DCore::QEntity * ceiling_;
-    Qt3DCore::QEntity * grid_;
+    std::unique_ptr<Table> table_;
     QVector<Qt3DCore::QEntity *> light_;
 };
